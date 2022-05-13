2fc1c1befee53083acc59f5380c2c9a334e3a498
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-13 13:04:58 +0800

    Create Management.h
    The first edition

---
+++ Management.h
@@ -0,0 +1,27 @@
+#ifndef MANAGEMENT_H
+#define MANAGEMENT_H
+#include "Worker.h"
+#include "Manager.h"
+#include "Employee.h"
+
+class Management
+{
+    public:
+        Management(); // default constructor
+        void Add_Staff(); // add new staff
+        void Display_Staff(); // display the information of staff
+        int IsExist(int id); // Determine whether staffs exist
+        void Delete_Staff(); // delete staff by seaching number
+        void Modify_Staff(); // modify the information of staff by searching number
+        void Search_Staff(); // check the information of staff by searching number
+        void Sort_Staff(); // put staff in order
+        void Show_menu(); // display interface of the management system
+        void ExitSystem(); // exit system;
+        ~Management(); // destructor
+        
+    protected:
+        int Emp_number; // record the current number of system
+        Worker** _EmpArray; // secondary pointer to record all employee data
+};
+
+#endif
