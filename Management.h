61983773a46246473861c98e99c2165ac9857e8e
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-17 09:26:59 +0800

    Update Management.h

--- Management.h
+++ Management.h
@@ -1,33 +1,27 @@
 #ifndef MANAGEMENT_H
 #define MANAGEMENT_H
-#include <iostream>
-#include <fstream>
-#define FILENAME "empFile.txt"
-#include "Worker.h"
-#include "Manager.h"
 #include "Employee.h"
+#include "Manager.h"
+#include "Worker.h"
 
-class Management
-{
-    public:
-        Management(); // default constructor
-        void Add_Staff(); // add new staff
-        void Display_Staff(); // display the information of staff
-        int IsExist(int id); // Determine whether staffs exist
-        void Delete_Staff(); // delete staff by seaching number
-        void Modify_Staff(); // modify the information of staff by searching number
-        void Search_Staff(); // check the information of staff by searching number
-        void Sort_Staff(); // put staff in order
-        void Show_menu(); // display interface of the management system
-        void ExitSystem(); // exit system;
-        void save();//save file
-        bool FileIsEmpty;//file not creat
-        int get_EmpNumber();
-        ~Management(); // destructor
-        
-    protected:
-        int Emp_number; // record the current number of system
-        Worker** _EmpArray; // secondary pointer to record all employee data
+class Management {
+ public:
+  Management();          // default constructor
+  void Add_Staff();      // add new staff
+  void Display_Staff();  // display the information of staff
+  int IsExist1(int id);   // Determine whether staffs exist by checking staff id
+  bool Management::IsExist2(std::string name); // Determine whether staffs exist by checking staff name
+  void Delete_Staff();   // delete staff by seaching number
+  void Modify_Staff();   // modify the information of staff by searching number
+  void Search_Staff();   // check the information of staff by searching number
+  void Sort_Staff();     // put staff in order
+  void Show_menu();      // display interface of the management system
+  void ExitSystem();     // exit system;
+  ~Management();         // destructor
 
+ protected:
+  int Emp_number;      // record the current number of system
+  Worker** _EmpArray;  // secondary pointer to record all employee data
 };
-#endif //MANAGEMENT_H
+
+#endif
