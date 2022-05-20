b21997cbc84f3889dabf786234f6282186247bd7
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-20 18:36:41 +0800

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -1,25 +1,19 @@
-#include <iostream>
-
 #include "Employee.h"
 
-Employee::Employee(int id, std::string name, int age) 
-{
-    this->_ID = id;
-    this->_Name = name;
-    this->_Age = age;
-    
+#include <iostream>
+
+Employee::Employee(int id, std::string name, int age) {
+  this->_Id = id;
+  this->_Name = name;
+  this->_Age = age;
 }
 
-std::string Employee::getDepartName() 
-{
-    return "Employee";
-}
-
-void Employee::showInformation() 
-{
-    std::cout << "Number: " << this->_ID
-              <<"\tName: " << this->_Name
-              << "\tAge: " << this->_Age
-              << "\tName of the post: " << this->getDepartName() 
-              << "\tJob: Complete tasks assigned by manager." << std::endl;
+std::string Employee::getDepartName() { return "Employee"; }
+int Employee::WorkTime() {return 8;}
+void Employee::showInformation() {
+  std::cout << "Number: " << this->_Id << "\tName: " << this->_Name
+            << "\tAge: " << this->_Age
+            << "\tName of the post: " << this->getDepartName()
+            << "\tJob: Complete tasks assigned by manager."
+            << "\tTime hours: "<<this->WorkTime()<<" hours." <<std::endl;
 }
