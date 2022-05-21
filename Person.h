f21264747e53cfdb2a5df18512aff754e8f48b45
chiahuic <90604140+chiahuic@users.noreply.github.com>
2022-05-21 10:55:03 +0800

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,12 +5,12 @@
 
 class Person {
  public:
-  virtual std::string getDepartName() = 0;  // show job title
-  virtual void showInformation() = 0;       // display information of persons
-  virtual int WorkTime();                  // person's working time range
-  int _Id;                                  // individual numbers of persons
-  std::string _Name;                        // name of persons
-  int _Age;                                 // Age of persons
+ virtual std::string getDepartName() = 0;  // show job title
+ virtual void showInformation() = 0;       // display information of persons
+ virtual int WorkTime();                   // person's working time range
+ int _Id;                                  // individual numbers of persons
+ std::string _Name;                        // name of persons
+ int _Age;                                 // Age of persons
 };
 
 #endif
