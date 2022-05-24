f7791b53d2ebd82bde10c49bb4551594f4d84880
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:46:39 +0800

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -2,10 +2,11 @@
 
 #include <iostream>
 
-Employee::Employee(int id, std::string name, int age) {
+Employee::Employee(int id, std::string name, int age, int D_id) {
   this->_Id = id;
   this->_Name = name;
   this->_Age = age;
+  this->_DepartmentID = D_id;
 }
 
 std::string Employee::getDepartName() { return "Employee"; }
@@ -14,6 +15,7 @@ void Employee::showInformation() {
   std::cout << "Number: " << this->_Id << "\tName: " << this->_Name
             << "\tAge: " << this->_Age
             << "\tName of the post: " << this->getDepartName()
+            << "\tPost ID: " << this->_DepartmentID
             << "\tJob: Complete tasks assigned by manager."
             << "\tTime hours: "<<this->WorkTime()<<" hours." <<std::endl;
 }
