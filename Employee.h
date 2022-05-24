99b06ca8f5fcaf89df568d455639eb3328f2042c
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 08:43:33 +0800

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -3,13 +3,16 @@
 
 #include "Person.h"
 // general employee class
-class Employee: public Person 
-{   
-    public: 
-        Employee(int id, std::string name, int age);
-        virtual std::string getDepartName() override; // override the worker class's function of the same name and get the department name of the employee
-        virtual void showInformation() override; // override the worker class's function of the same name and let the general employee class display the information
-        
+class Employee : public Person {
+ public:
+  Employee(int id, std::string name, int age);
+  virtual std::string getDepartName()
+      override;  // override the worker class's function of the same name and
+                 // get the department name of the employee
+  virtual void showInformation()
+      override;  // override the worker class's function of the same name and
+                 // let the general employee class display the information
+  virtual int WorkTime() override; // The number of hours worked per day by an employee
 };
 
-#endif //EMPLOYEE_H
+#endif
