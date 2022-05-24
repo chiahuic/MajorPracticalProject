6acfd3d8c585ae572872992a180a79302e472775
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:47:19 +0800

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -2,10 +2,11 @@
 
 #include <iostream>
 
-Manager::Manager(int id, std::string name, int age) {
+Manager::Manager(int id, std::string name, int age, int D_id) {
   this->_Id = id;
   this->_Name = name;
   this->_Age = age;
+  this->_DepartmentID = D_id;
 }
 
 std::string Manager::getDepartName() { return "Manager"; }
@@ -14,6 +15,8 @@ void Manager::showInformation() {
   std::cout << "Number: " << this->_Id << "\tName: " << this->_Name
             << "\tAge: " << this->_Age
             << "\tName of the post: " << this->getDepartName()
+            << "\tPost ID: " << this->_DepartmentID
             << "\tJob: Assign tasks to employees." 
             << "\tTime hours: "<<this->WorkTime()<<" hours." <<std::endl;
 }
+
