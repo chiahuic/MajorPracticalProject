85517c841050ae3795298dad807f682a49cf9aeb
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 08:45:37 +0800

    Update Management.h

--- Management.h
+++ Management.h
@@ -7,22 +7,23 @@
 class Management {
  public:
   Management();          // default constructor
-  void Add_Staff();      // add new staff
+  void Add_Staff();      // add new staff that include name,age and staff number
   void Display_Staff();  // display the information of staff
-  int IsExist1(int id);   // Determine whether staffs exist by checking staff id
-  bool IsExist2(std::string name); // Determine whether staffs exist by checking staff name
-  void Delete_Staff();   // delete staff by seaching number
-  void Modify_Staff();   // modify the information of staff by searching number
-  void Search_Staff();   // check the information of staff by searching number
-  void Sort_Staff();     // put staff in order
-  void Show_menu();      // display interface of the management system
-  void Run_System();     // After the user enters the options, the interface for each function is displayed
-  void ExitSystem();     // exit system;
-  ~Management();         // destructor
+  int IsExist1(int id);  // Determine whether staffs exist by checking staff id
+  bool IsExist2(std::string name);  // Determine whether staffs exist by
+                                    // checking staff name
+  void Delete_Staff();              // delete staff by seaching number
+  void Modify_Staff();  // modify the information of staff by searching number
+  void Search_Staff();  // check the information of staff by searching number
+  void Sort_Staff();    // In ascending order of staff number
+  void Show_menu();     // display interface of the management system
+  void Run_System();    // After the user enters the options, the interface for each function is displayed
+  void ExitSystem();    // exit system;
+  ~Management();        // destructor
 
  protected:
-  int Emp_number;      // record the current number of system
-  Person** _EmpArray;  // secondary pointer to record all employee data
+  int _StaffNumber;      // record the current number of system
+  Person** _StaffArray;  // secondary pointer to record all employee data
 };
 
 #endif
